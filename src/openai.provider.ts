446313f2f3f0d92555a4f1ec216b7ff4d1e0f524
Alex Duan <alex.duan@dynamo.works>
2026-02-23 17:37:03 -0500

    updated getModelNameFromRequest to return string | undefined since OpenAI has model as optional

--- src/openai.provider.ts
+++ src/openai.provider.ts
@@ -61,8 +61,8 @@ export class OpenAIProvider extends BaseProvider<OpenAI, ResponseCreateParamsBas
         return this.responseFactory.createError(error.message, error.code ? error.code : undefined);
     }
 
-    async getModelNameFromRequest(payload: ResponseCreateParamsBase | ChatCompletionCreateParamsBase): Promise<string> {
-        return payload.model ?? '' as string;
+    async getModelNameFromRequest(payload: ResponseCreateParamsBase | ChatCompletionCreateParamsBase): Promise<string | undefined> {
+        return payload.model;
     }
 
     protected async handleRequest(
